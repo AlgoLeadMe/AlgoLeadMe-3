ba2e6dd78ddbd296f2659da7ef8716d21a1aaffa
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2023-11-30 00:34:05 +0900

    Update 8-gjsk132 pyfile

--- gjsk132/DP/1463 1로 만들기.py
+++ gjsk132/DP/1463 1로 만들기.py
@@ -3,9 +3,7 @@ dp = [1000001 for _ in range(1000001)]
 dp[0] = 1000001
 dp[1] = 0
 
-i = 2
-
 for i in range(2, target+1):
-    dp[i] = min(dp[i//3] if i%3 == 0 else 100001, dp[i//2] if not i%2 else 10001, dp[i-1])+1
+    dp[i] = min(dp[i//3] if not i%3  else 1000001, dp[i//2] if not i%2 else 1000001, dp[i-1])+1
 
 print(dp[target])
