4ce6a4d471f424e43f355b3fec177fe828318675
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-01-13 07:03:33 +0900

    Update 17485 진우의 달 여행 (Large).py

--- gjsk132/DP/17485 진우의 달 여행 (Large).py
+++ gjsk132/DP/17485 진우의 달 여행 (Large).py
@@ -2,12 +2,11 @@ input = open(0).readline
 N, M = map(int,input().split())
 dp = [[[i]*3 for i in map(int,input().split())] for _ in range(N)]
 
-for i in range(0, N-1):
+for i in range(1, N):
     for j in range(M):
-        before = dp[i]
-        dp[i+1][j][-1] = 10**7 if j==M-1 else ( dp[i+1][j][-1] + min(before[j][0], before[j+1][1]) )
-        dp[i+1][j][0] += min(10**7 if j==0 else before[j-1][-1], 10**7 if j==M-1 else before[j+1][1])
-        dp[i+1][j][1] = 10**7 if j==0 else ( dp[i+1][j][1] + min(before[j-1][-1], before[j][0]) )
+        dp[i][j][-1] = 10**7 if j==0 else ( dp[i][j][-1] + min(dp[i-1][j][0], dp[i-1][j-1][1]) )
+        dp[i][j][0] += min(10**7 if j==0 else dp[i-1][j-1][1], 10**7 if j==M-1 else dp[i-1][j+1][-1])
+        dp[i][j][1] = 10**7 if j==M-1 else ( dp[i][j][1] + min(dp[i-1][j+1][-1], dp[i-1][j][0]) )
 
 low = 10**7
 
